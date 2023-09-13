8b8d63672da293b89c1a24c6222c420eda00d753
Rumi Jahan Dalas <rumi@bd.batworld.com>
2023-09-13 16:56:44 +0600

    update

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { EditorComponent } from '@tinymce/tinymce-angular';
 
 @Component({
   selector: 'app-layout',
@@ -6,7 +7,53 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements AfterViewInit {
-  content:any
+  content: any
+
+  config: EditorComponent['init'] = {
+    height: 500,
+
+    plugins: [
+      'a11ychecker',
+      'advlist',
+      'advcode',
+      'advtable',
+      'autolink',
+      'checklist',
+      'export',
+      'lists',
+      'link',
+      'image',
+      'charmap',
+      'preview',
+      'anchor',
+      'searchreplace',
+      'visualblocks',
+      'powerpaste',
+      'fullscreen',
+      'formatpainter',
+      'insertdatetime',
+      'media',
+      'table',
+      'help',
+      'wordcount'
+    ],
+    toolbar:
+      'export|undo redo | casechange blocks | bold italic backcolor | \
+  alignleft aligncenter alignright alignjustify | \
+  bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
+
+    image_title: true,
+
+    automatic_uploads: true,
+
+    file_picker_types: 'image',
+    file_picker_callback: function (cb, value, meta) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+      input.click();
+    },
+  };
 
 
 
@@ -14,13 +61,13 @@ export class LayoutComponent implements AfterViewInit {
 
   }
 
-  downloadPdf(){
+  downloadPdf() {
     console.log('called')
   }
 
 
-  handleEvent(event:any){
-    console.log('clicked' , event)
+  handleEvent(event: any) {
+    console.log('clicked', event)
 
   }
 
