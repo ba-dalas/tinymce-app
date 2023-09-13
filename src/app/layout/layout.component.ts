1aada59c2eb711556aaabb6818a2f83f9ba2bad5
Rumi Jahan Dalas <rumi@bd.batworld.com>
2023-09-13 18:06:46 +0600

    init

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { EditorComponent } from '@tinymce/tinymce-angular';
 
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -8,6 +9,8 @@ import { EditorComponent } from '@tinymce/tinymce-angular';
 })
 export class LayoutComponent implements AfterViewInit {
   content: any
+   file! :File
+
 
   config: EditorComponent['init'] = {
     height: 500,
@@ -35,7 +38,9 @@ export class LayoutComponent implements AfterViewInit {
       'media',
       'table',
       'help',
-      'wordcount'
+      'wordcount',
+      'code'
+
     ],
     toolbar:
       'export|undo redo | casechange blocks | bold italic backcolor | \
@@ -48,9 +53,38 @@ export class LayoutComponent implements AfterViewInit {
 
     file_picker_types: 'image',
     file_picker_callback: function (cb, value, meta) {
-      var input = document.createElement('input');
+      let input = document.createElement('input');
       input.setAttribute('type', 'file');
       input.setAttribute('accept', 'image/*');
+      //
+
+      input.onchange = () => {
+        const file = input.files?.[0];
+
+        if (file) {
+          console.log('file', file)
+          const reader = new FileReader();
+          reader.onload = () => {
+            const id = 'blobid' + new Date().getTime();
+            console.log('id', id)
+            // const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+            const base64 = reader.result?.toString().split(',')[1];
+            console.log('base64', base64)
+
+            // if (base64) {
+            //   const blobInfo = blobCache.create(id, file, base64);
+            //   blobCache.add(blobInfo);
+
+            //   cb(blobInfo.blobUri(), { title: file.name });
+            // }
+          };
+
+          reader.readAsDataURL(file);
+        }
+      };
+
+
+      //
       input.click();
     },
   };
